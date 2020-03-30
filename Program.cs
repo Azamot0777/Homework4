64ca5391fef7d71c0af0b8640abd49322fe82ff8
Heisenberg <Persey_98@list.ru>
2020-03-30 16:28:07 +0500

    Homework4

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Console.WriteLine ("ЗАДАНИЕ 2а");
+            // Console.Write ("a = ");
+            // int a = int.Parse (Console.ReadLine ());
+            // Console.Write ("b = ");
+            // int b = int.Parse (Console.ReadLine ()); 
+            // int sum = 0;
+            // --b;
+
+            // while (a < b)
+            // {
+            //     ++a;
+            //     sum += a;
+            // }
+            // Console.WriteLine ($"sum = {sum}");
+
+
+
+            // Console.WriteLine ("ЗАДАНИЕ 2b");
+            // Console.Write ("a = ");
+            // int a = int.Parse (Console.ReadLine ());
+            // Console.Write ("b = ");
+            // int b = int.Parse (Console.ReadLine ());
+            // a++;
+            // if (a != b) 
+            // {
+            //     for (int i = a + 1; i <= b - 1; i++)
+            //     {
+            //         if (i % 2 == 1)
+            //         {
+            //             Console.WriteLine(i);
+            //         }
+            //     }
+            // }
+            // else 
+            // {
+            //     Console.WriteLine ("Нечетных чисел нет");
+            // }
+                    
+
+
+            // Console.WriteLine ("ЗАдаНие 3");
+            // Console.WriteLine ("Прямоугольник");
+            // for (int i = 1; i < 20; i++) Console.Write ("*"); 
+            // Console.WriteLine ("");
+            // for (int i = 0; i < 4; i++) Console.WriteLine ("*       " + "          *");
+            // for (int i = 1; i < 20; i++) Console.Write("*");
+            
+
+
+            // Console.WriteLine ("Прямоугольный треугольник");
+            // int a = 0;
+
+            // for (int i = 0; i < 5 ; i++) 
+            // {
+            //     Console.WriteLine("*");
+            //     for (int x = 0; x <= a ; x++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.Write ("*");
+            //     a += 2;
+            //     Console.WriteLine ( );
+            // }
+
+            // for (int i = 0; i <= 10; i++) Console.Write ("*");
+
+
+
+            // Console.WriteLine ("Равносторонний треугольник");
+
+            // int a = 10;
+            // int b = 0;
+
+            // for (int i = 0; i <= 10; i++)
+            // {
+            //     for (int t = 0; t <= a; t++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.Write ("*");
+            //     for (int x = 0; x < b; x++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.WriteLine ("*");
+            //     a--;
+            //     b += 2;
+            // }
+
+            // for (int i = 0; i < 24; i++) Console.Write ("*");
+
+
+
+
+            // Console.WriteLine ("РоМБ");      
+            // int a = 10;
+            // int b = 0;
+
+            // for (int i = 0; i <= 10; i++)
+            // {
+            //     for (int t = 0; t <= a; t++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.Write ("*");
+            //     for (int x = 0; x < b; x++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.WriteLine ("*");
+            //     a--;
+            //     b += 2;
+            // }
+            
+            // for (int i = 0; i <= 11; i++)
+            // {
+            //     for (int t = 0; t <= a; t++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.Write ("*");
+            //     for (int x = 0; x < b; x++)
+            //     {
+            //         Console.Write (" ");
+            //     }
+            //     Console.WriteLine ("*");
+            //     a++;
+            //     b -= 2;
+            // }
+
+
+
+            // Console.WriteLine ("ЗАданИЕ 4");
+
+            // double N = 1000;
+            // double p = 3.5;
+            // int k = 0;
+            // while (N <= 1100)
+            // {
+                
+            //     N += N * p / 100;
+            //     k++;
+            // }
+            // Console.WriteLine ($"месяц = {k}");
+            // Console.WriteLine ($"Сумма = {N}");
+
+            Console.ReadKey ();
+        }
+    }
+}
